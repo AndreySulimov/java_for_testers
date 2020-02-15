f2710d7a1dafe89a225a4d62199c6ff74498edbd
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-02-15 11:56:08 +0300

    Доработано задание 2

--- sandbox/src/main/java/ru/jft/sandbox/Dz2.java
+++ sandbox/src/main/java/ru/jft/sandbox/Dz2.java
@@ -4,14 +4,12 @@ public class Dz2 {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point (1, 1);
-    Point p2 = new Point (5, 5);
-    System.out.println("Расстояние между точками с координатами "
-            + "(" + p1.x1 + ", " + p1.y1 + ") " + "и " + "(" + p2.x2 + ", " + p2.y2 + ") "
-            + "= " + distance(p1, p2));
-  }
+    Point p1 = new Point(3, 2);
+    Point p2 = new Point(7, 8);
 
-  public static double distance (Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x2 - p1.x1), 2) + Math.pow((p2.y2 - p1.y1), 2));
+    System.out.println("Расстояние между точками с координатами "
+            + "(" + p1.x + ", " + p1.y + ") " + "и " + "(" + p2.x + ", " + p2.y + ") "
+            + "= " + p1.distance(p2));
+
   }
 }
