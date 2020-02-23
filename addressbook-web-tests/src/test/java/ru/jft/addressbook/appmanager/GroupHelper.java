8b94764bab5b1beb67193594cef7ad420c060591
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-02-23 20:18:46 +0300

    Задание 7
    Реализована возможность запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/jft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/appmanager/GroupHelper.java
@@ -1,12 +1,12 @@
 package ru.jft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 import ru.jft.addressbook.model.GroupData;
 
 public class GroupHelper extends HelperBase {
 
-  public GroupHelper(FirefoxDriver wd) {
+  public GroupHelper(WebDriver wd) {
     super(wd);
   }
 
--- addressbook-web-tests/src/test/java/ru/jft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/appmanager/HelperBase.java
@@ -3,12 +3,12 @@ package ru.jft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 
 public class HelperBase {
-  public FirefoxDriver wd;
+  public WebDriver wd;
 
-  public HelperBase(FirefoxDriver wd) {
+  public HelperBase(WebDriver wd) {
     this.wd = wd;
   }
 
