edbffbcd4d836b6c053b539ab728b675cbd88a9b
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-02-24 13:04:40 +0300

    Реализована проверка и обеспечение предусловий в тестах для удаления и модификации групп

--- addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupCreationTests.java
@@ -8,9 +8,6 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("Test1", "Test1", "Test1"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("Test1", "Test1", "Test1"));
   }
 }
--- addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.jft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.jft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Test1", "Test1", "Test1"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
