9a0101b164eeac144a378bf11b7b404dea69b364
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-03-02 21:55:20 +0300

    Проверка на количество групп
    В тесты для создания, удаления и модификации групп добавлена проверка на количество групп

--- addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.jft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.jft.addressbook.model.GroupData;
 
@@ -8,6 +9,9 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("Test1", "Test1", "Test1"));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before + 1);
   }
 }
--- addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.jft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.jft.addressbook.model.GroupData;
 
@@ -8,11 +9,14 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Test1", "Test1", "Test1"));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
   }
 }
