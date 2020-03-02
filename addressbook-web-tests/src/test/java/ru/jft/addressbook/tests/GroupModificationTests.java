5af74e94450b7e795b49f6aec32fddc8c34030fd
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-03-02 22:02:37 +0300

    Выбор групп по индексу
    Добавлена возможность удаления и модификации групп в соответствии с их индексом

--- addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Test1", "Test1", "Test1"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Test2", "Test2", "Test2"));
     app.getGroupHelper().submitGroupModification();
