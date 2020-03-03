c3008b41ca1182987ce24349020d28ed5458c65e
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-03-03 22:20:06 +0300

    В тесты для создания и модификации групп добавлена сортировка списков

--- addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupDeletionTests.java
@@ -11,7 +11,7 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()) {
+    if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Test1", "Test1", "Test1"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
