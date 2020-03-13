dc78432ba37b6ec819248176cdc03451a6c4da3e
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-03-13 23:23:40 +0300

    В тесты для групп добавлено использование уникальных идентификаторов объектов

--- addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupCreationTests.java
@@ -4,25 +4,31 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.jft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.goTo().groupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData().withName("Test2");
     app.group().create(group);
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() + 1);
 
     group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    /*вычисляем максимальный идентификатор:
+    берем коллекцию, содержащую группы с уже известными идентификаторами,
+    превращаем ее в поток
+    и этот поток объектов типа GroupData превращаем в поток идентификаторов с помощью mapToInt.
+    В качестве параметра в mapToInt передаем анонимную функцию, которая последовательно применяется
+    ко всем элементам потока и каждый из них последовательно преобразуется в число.
+    Затем вызываем метод max() для получения максимального из чисел и преобразуем результат в
+    обычное целое число - getAsInt() - это и будет максимальный из идентификаторов*/
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
+
     before.add(group);
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
     Assert.assertEquals(before, after);
   }
 }
--- addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/tests/GroupDeletionTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.jft.addressbook.model.GroupData;
 
-import java.util.List;
+import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -13,20 +13,20 @@ public class GroupDeletionTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     // проверка существования хотя бы одной группы: если список пустой, то группу нужно создать
-    if (app.group().list().size() == 0) {
+    if (app.group().all().size() == 0) {
       app.group().create(new GroupData().withName("Test1"));
     }
   }
 
   @Test
   public void testGroupDeletion() throws Exception {
-    List<GroupData> before = app.group().list(); // запоминаем содержимое списка перед удалением
-    int index = before.size() - 1; // индекс удаляемой группы (удаляем последнюю группу)
-    app.group().delete(index);
-    List<GroupData> after = app.group().list();
+    Set<GroupData> before = app.group().all(); // запоминаем содержимое списка перед удалением
+    GroupData deletedGroup = before.iterator().next();
+    app.group().delete(deletedGroup);
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() - 1);
 
-    before.remove(index);
+    before.remove(deletedGroup);
     Assert.assertEquals(before, after);
   }
 }
