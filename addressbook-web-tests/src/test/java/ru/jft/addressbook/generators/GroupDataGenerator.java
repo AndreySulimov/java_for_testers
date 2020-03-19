9d383f93d87d3889538406e320b10dad11c485da
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-03-19 22:03:10 +0300

    В GroupDataGenerator добавлено использование библиотеки jcommander
    Анализ опций командой строки при запуске генератора данных

--- addressbook-web-tests/src/test/java/ru/jft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/jft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.jft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.jft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -11,16 +14,31 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
   public static void main(String[] args) throws IOException {
 
-    int count = Integer.parseInt(args[0]); // количество групп
-    File file = new File(args[1]); // путь к файлу
-
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException { // сохранение списка в файл
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
+
+  private void save(List<GroupData> groups, File file) throws IOException { // сохранение списка в файл
     System.out.println(new File(".").getAbsolutePath());
     // открываем файл на запись
     Writer writer = new FileWriter(file);
@@ -31,7 +49,7 @@ public class GroupDataGenerator {
     writer.close(); // закрываем файл
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>(); // создаем новый список объектов типа GroupData
     // заполняем список заданным количеством групп
     for (int i = 0; i < count; i++) {
