053d9f1211a6677a9a08af0ee4644defd647f603
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-04-11 21:56:43 +0300

    Испорчен тест для проверки Jenkins

--- sandbox/src/test/java/ru/jft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/jft/sandbox/SquareTests.java
@@ -9,6 +9,6 @@ public class SquareTests {
   // создаем метод testArea, который не имеет возвращаемого значения
   public void testArea() {
     Square s = new Square(5); // создаем новый квадрат со стороной 5
-    Assert.assertEquals(s.area(), 25.0); // сравниваем вычисленную и ожидаему площадь
+    Assert.assertEquals(s.area(), 20.0); // сравниваем вычисленную и ожидаему площадь
   }
 }
